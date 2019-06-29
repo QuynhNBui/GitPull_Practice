bc608d3e9d08dcbe9bc3cfad1d530a8ae81cbcc6
kylietan <50597718+kylietan@users.noreply.github.com>
2019-06-29 11:09:21 -0700

    Adding jelly_bean_counter.py

---
+++ jelly_bean_counter.py
@@ -0,0 +1,6 @@
+# This is a function that counts the numebr of jelly beans in a list
+def jelly_bean_counter(lst):
+    jelly_bean_count = 0
+    for item in lst:
+        jelly_bean_count += item
+    return jelly_bean_count
